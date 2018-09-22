1f5a41c5cf852847928acafd1625b6e51b7351c7
esgaroth84 <37635491+esgaroth84@users.noreply.github.com>
2018-09-22 11:49:39 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) 
+	{
+		asa
+		
+	}
+
+	public static void main(String[] args)
+{
+		launch(args);
+	}
+}
